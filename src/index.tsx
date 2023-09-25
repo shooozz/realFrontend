86dc0e80c37b485fc3271935a0de6c6520b44744
shooozz <vodolb@mail.ru>
2023-09-25 21:11:16 +0300

    Created

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import './index.css';
+
+import App from './App';
+
+const container = document.getElementById('root') as HTMLDivElement;
+const root = createRoot(container!);
+
+root.render(<App />);
